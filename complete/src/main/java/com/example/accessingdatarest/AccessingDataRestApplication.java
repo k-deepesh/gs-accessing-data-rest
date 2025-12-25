42c78aa41e05ee7ed04d76ea027eab9e6d680ec3
Deepesh <deepeshkumar8495@gmail.com>
2025-12-25 21:30:59 +0530

    Customize greeting response message

--- complete/src/main/java/com/example/accessingdatarest/AccessingDataRestApplication.java
+++ complete/src/main/java/com/example/accessingdatarest/AccessingDataRestApplication.java
@@ -8,5 +8,7 @@ public class AccessingDataRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccessingDataRestApplication.class, args);
+		System.out.println("Application complete");
+		
 	}
 }
